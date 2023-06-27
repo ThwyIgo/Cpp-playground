e3c392fcc6a09be2704b5ca2f0ee87cbda82083b
ThwyIgo <thiagopachecorocha@hotmail.com>
2023-06-26 22:01:24 -0300

    const const const

--- graph.cpp
+++ graph.cpp
@@ -36,7 +36,7 @@ public:
         std::make_heap(data.begin(), data.end(), cmpfunc);
     }
 
-    bool empty(void) {
+    bool empty(void) const {
         return data.empty();
     }
 };
@@ -54,73 +54,76 @@ class Graph {
                       std::shared_ptr<std::unordered_map<
                                           T,std::optional<T>>>
                       > dijkstra_t;
-
-    constexpr static W infinity(void) {
-        if (std::numeric_limits<W>::has_infinity)
-            return std::numeric_limits<W>::infinity();
-        return std::numeric_limits<W>::max();
-    }
         
-    //                                           value / weight
-    std::unordered_map<T, std::forward_list<std::pair<T, W>>> adjVerts;
+    // The only attribute of this class
+    std::unordered_map<T, std::forward_list<std::pair<T, W>>> verts;
 
 public:
     Graph(void) {}
 
     explicit Graph(size_t reserve) {
-        adjVerts.reserve(reserve);
+        verts.reserve(reserve);
     }
 
     // O(n) : n = edges
-    Graph(std::initializer_list<std::pair<T,std::initializer_list<std::pair<T,W>>>> il) {
-        adjVerts.reserve(il.size());
+    // Doesn't check if edges are duplicated
+    constexpr Graph(std::initializer_list<std::pair<T,std::initializer_list<std::pair<T,W>>>> il) {
+        verts.reserve(il.size());
         for (auto [k,v] : il)
             for (auto p : v)
-                adjVerts[k].push_front(p);
+                verts[k].push_front(p);
     }
 
     // O(n) : n = edges
     bool addEdge(T origin, T destination, W weight) {
-        auto &list = adjVerts[origin];
+        auto &list = verts[origin];
         if (std::find_if(list.begin(), list.end(),
                          eqEdge(destination))
             != list.end())
             return false;
         
         list.push_front({destination, weight});
+        return true;
+    }
+
+    // O(1)
+    bool addEdge(T origin, T destination, W weight, bool iSwearThisIsntDuplicate) {
+        if (iSwearThisIsntDuplicate == false)
+            return addEdge(origin, destination, weight);
         
+        verts[origin].push_front({destination, weight});
         return true;
     }
 
     // O(n)
     bool removeEdge(T origin, T destination) {
-        return std::erase_if(adjVerts[origin],
+        return std::erase_if(verts[origin],
                              eqEdge(destination));
     }
 
     // O(n) : n = origin's adjacent vertices
     auto& getPairRef(T origin, T destination) {
-        return *std::find_if(adjVerts[origin].begin(), adjVerts[origin].end(),
+        return *std::find_if(verts[origin].begin(), verts[origin].end(),
                              eqEdge(destination));
     }
     
     // O(1)
-    size_t size(void) {
-        return adjVerts.size();
+    size_t size(void) const {
+        return verts.size();
     }
 
     // O(v * e) : v = vertices : e = edges
-    dijkstra_t dijkstra(T origin) {
+    dijkstra_t dijkstra(T origin) const {
         // (*pprev)[v] == previous vertex of v in the path
         auto pprev = std::make_shared<std::unordered_map<T,std::optional<T>>>();
         auto &prev = *pprev;
-        prev.reserve(adjVerts.size());
+        prev.reserve(verts.size());
         heap_min heapMin;
         // dist[v] == distance from origin to v
         std::unordered_map<T, std::shared_ptr<W>> dist;
-        dist.reserve(adjVerts.size());
+        dist.reserve(verts.size());
 
-        for (const auto &[v,_] : adjVerts) {
+        for (const auto &[v,_] : verts) {
             if (v != origin) {
                 dist[v] = std::make_shared<W>(infinity());
                 heapMin.push({v, dist[v]});
@@ -132,8 +135,9 @@ public:
 
         while (!heapMin.empty()) {
             const auto [u,uw] = heapMin.pop();
+            const auto &[_,adjVerts] = *verts.find(u);
 
-            for (const auto &[v,w] : adjVerts[u]) {
+            for (auto &[v,w] : adjVerts) {
                 W tempDist = *uw + w;
 
                 if (dist[v] == nullptr)
@@ -166,9 +170,15 @@ public:
             std::cout << v << '/' << w << '\n';
         }
     }
+
+    constexpr static W infinity(void) {
+        if (std::numeric_limits<W>::has_infinity)
+            return std::numeric_limits<W>::infinity();
+        return std::numeric_limits<W>::max();
+    }
     
 private:    
-    auto eqEdge(const T &destination) {
+    auto static eqEdge(const T &destination) {
         return [&](const std::pair<T,W> &pair)
             {return pair.first == destination;};
     }
@@ -184,8 +194,8 @@ private:
     }
 
 public: // Debug, will be deleted
-    void print(void) {
-        for (auto &[k,l] : adjVerts) {
+    void print(void) const {
+        for (auto &[k,l] : verts) {
             std::cout << k << " -> ";
             for (auto &[v,w] : l)
                 std::cout << ", " << v << '/' << w;
@@ -230,8 +240,9 @@ int main(void)
     cout << "\nVert count: " << meuGrafo.size() << '\n';
     meuGrafo.print();
 
-    cout << "\nRemoving Beraba from Udia\n";
+    cout << "\nRemoving Beraba from Udia and adding Araguari to Beraba\n";
     meuGrafo.removeEdge("Udia", "Beraba");
+    meuGrafo.addEdge("Beraba", "Araguari", 40, true);
     meuGrafo.print();
     
     cout << "\nChanging an element\n";
